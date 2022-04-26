6ce9bd74a2d2eaeb3af9c3a7c070e9c47f3a8d3e
sumit8692 <sumit8692@gmail.com>
2022-04-26 15:32:16 +0530

    Time: 6 ms (15.39%), Space: 8.5 MB (17.79%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -14,15 +14,17 @@ class Solution {
 public:
     
     void inorder(TreeNode* root){
-        if(!root){
+        if(root == NULL)
             return ;
-        }
+            
         inorder(root->left);
         ans.push_back(root->val);
         inorder(root->right);
         
     }
+    
     vector<int> inorderTraversal(TreeNode* root) {
+        
         inorder(root);
         return ans;
         
