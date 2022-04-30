1fba7f9408e0c92bb2f3906f27d684d385370bcb
sumit8692 <sumit8692@gmail.com>
2022-04-30 18:47:30 +0530

    Time: 11 ms (78.30%), Space: 8.1 MB (57.12%) - LeetHub

---
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+//floyd loop detection cycle
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        
+        if(head == NULL)    return false;
+        
+        ListNode *slow = head;
+        ListNode *fast = head;
+        
+        while(fast!=NULL && fast->next!=NULL){
+            
+            fast = fast->next->next;
+            slow = slow->next;
+    
+            if(fast == slow)    return true;
+        }
+        return false;
+    }
+};
