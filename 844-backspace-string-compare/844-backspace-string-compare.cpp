4eab6284f9e01c6d26d705445247d25c6e0c8bc4
sumit8692 <sumit8692@gmail.com>
2022-05-01 22:43:25 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (88.17%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -1,16 +1,65 @@
+// class Solution {
+// public:
+//     bool backspaceCompare(string s, string t) {
+//         stack<char> s1, s2;
+        
+//         for(int i = 0; i < s.size(); i++){
+//             if(s[i]=='#' and !s1.empty())   s1.pop();
+// 			else if(s[i]!='#')  s1.push(s[i]);
+// 		}
+// 		for(int i=0;i<t.size();i++){
+// 			if(t[i]=='#' and !s2.empty())   s2.pop();
+// 			else if(t[i]!='#')  s2.push(t[i]);
+// 		}
+// 		return s1==s2;
+        
+        
+        
+        
+        
+        
+// 	}
+// };
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        stack<char> s1, s2;
-        
-        for(int i = 0; i < s.size(); i++){
-            if(s[i]=='#' and !s1.empty())   s1.pop();
-			else if(s[i]!='#')  s1.push(s[i]);
-		}
-		for(int i=0;i<t.size();i++){
-			if(t[i]=='#' and !s2.empty())   s2.pop();
-			else if(t[i]!='#')  s2.push(t[i]);
-		}
-		return s1==s2;
-	}
+        int j=0,k=0;
+        // for 1st string
+        for(int i=0;i<s.size();i++)
+        {
+           if(s[i]=='#')
+           {
+                j--;
+                j=max(0,j); // j will never be -ve
+           }
+           else
+           {
+               s[j]=s[i];
+               j++;
+           }
+        }
+        // for 2nd string
+        for(int i=0;i<t.size();i++)
+        {
+           if(t[i]=='#')
+           {
+                k--;
+                k=max(0,k); // k will never be -ve
+           }
+           else
+           {
+               t[k]=t[i];
+               k++;
+           }
+        }
+        if(j!=k) return false;
+        else
+        {
+            for(int i=0;i<k;i++)
+            {
+                if(s[i]!=t[i]) return false;
+            }
+            return true;
+        }
+    }
 };
