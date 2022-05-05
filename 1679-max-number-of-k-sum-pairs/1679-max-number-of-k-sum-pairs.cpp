015d336dc087bb32d6f98d9adfc87554df22d56a
sumit8692 <sumit8692@gmail.com>
2022-05-05 07:18:27 +0530

    Time: 141 ms (88.12%), Space: 58.1 MB (97.24%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -2,23 +2,39 @@ class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
         
-        int n = nums.size();
-        unordered_map<int, int> freqMap;
-        int ans = 0;
+//         int n = nums.size();
+//         unordered_map<int, int> freqMap;
+//         int ans = 0;
         
-        for(int i = 0 ; i < n ; i++){
+//         for(int i = 0 ; i < n ; i++){
             
-            if(freqMap[k - nums[i]] > 0){
-                ans++;
-                freqMap[k-nums[i]]--;
-            }else{
-                freqMap[nums[i]]++;
+//             if(freqMap[k - nums[i]] > 0){
+//                 ans++;
+//                 freqMap[k-nums[i]]--;
+//             }else{
+//                 freqMap[nums[i]]++;
+//             }
+            
+//         }
+        
+//         return ans;
+        
+        sort(nums.begin(), nums.end());
+        int i = 0;
+        int j = nums.size() - 1;
+        int count = 0;
+        while(i<j){
+            
+            if(nums[i] + nums[j] == k){
+                count++;
+                i++;
+                j--;
             }
             
+            else if(nums[i] + nums[j] > k)   j--;
+            else i++;
+            
         }
-        
-        return ans;
-        
-        
+        return count;
     }
 };
