62a3ef28f0a3f5d609f5494ec4a78d125f57c5ed
sumit8692 <sumit8692@gmail.com>
2022-05-10 02:59:18 +0530

    Time: 13 ms (60.47%), Space: 21.2 MB (92.68%) - LeetHub

--- 112-path-sum/112-path-sum.cpp
+++ 112-path-sum/112-path-sum.cpp
@@ -13,11 +13,11 @@ class Solution {
 public:
     bool hasPathSum(TreeNode* root, int targetSum) {
         
-        if(root == NULL)    return false;
+        if(root == NULL)    return false; // check if root is null then return false
         
-        else if(root->left == NULL && root->right == NULL && targetSum-root->val==0) return 1;
+        else if(root->left == NULL && root->right == NULL && targetSum-root->val==0) return 1; // if both left and right is null and our targetsum becomes 0 then keep returning true.
         
-        else return hasPathSum(root->left,targetSum-root->val) || hasPathSum(root->right,targetSum-root->val);
+        else return hasPathSum(root->left,targetSum-root->val) || hasPathSum(root->right,targetSum-root->val); // keep calling same function for both left and right node until null node is reached. We have used or because if any path return true then that's our answer... 
         
         
     }
