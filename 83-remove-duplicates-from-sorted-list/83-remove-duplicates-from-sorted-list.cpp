5e5929d2e34ae3bdcfbd31fad4239709d8090479
sumit8692 <sumit8692@gmail.com>
2022-05-10 05:11:57 +0530

    Time: 14 ms (52.89%), Space: 11.6 MB (37.05%) - LeetHub

--- 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.cpp
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.cpp
@@ -12,18 +12,27 @@ class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
         
-        if(head == NULL || head->next == NULL)  return head;
-        ListNode* temp = head;
-        while(temp->next != NULL){
+//         if(head == NULL || head->next == NULL)  return head; // if there is no node or a single node then there's no question of duplicates.
+//         ListNode* temp = head; // declaring a temp pointer
+//         while(temp->next != NULL){
             
-            if(temp->val == temp->next->val){
-                ListNode *del_node = temp->next;
-                temp->next = del_node->next;
-                delete del_node;
-            }
+//             if(temp->val == temp->next->val){  
+//                 ListNode *del_node = temp->next;
+//                 temp->next = del_node->next;  /// removing the link between nodes
+//                 delete del_node;
+//             }
             
-            else    temp = temp->next;   
-        }
-        return head;
+//             else    temp = temp->next; //keep moving if no duplicates
+//         }
+//         return head;
+         if(head == NULL || head->next == NULL) return head;
+    
+         ListNode* newhead = deleteDuplicates(head->next);
+        
+         if(head->val == newhead->val)  return newhead;
+         else{
+             head->next = newhead;
+             return head;
+         }
     }
 };
