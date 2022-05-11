ff5001e16511faca07ae8ace654ed3b39d206ce9
sumit8692 <sumit8692@gmail.com>
2022-05-12 00:44:50 +0530

    Time: 213 ms (19.62%), Space: 93.3 MB (90.12%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-//         int maxstock = 0;
-//         for(int i = 0; i < prices.size()-1; i++)
-//             for(int j = i+1; j < prices.size(); j++)
-//                 if(prices[j] > prices[i])   maxstock = max(prices[j] - prices[i],maxstock);
-            
-//         return maxstock;
-        // NIBBA Solution
         
-        
-        
-        int minprice = INT_MAX;
-        int maxprof = 0;
+        //int st = 0;
+        int maxprofit = 0;
+        int ans = INT_MAX;
         for(int i = 0; i < prices.size(); i++){
-            minprice = min(minprice, prices[i]);
-            maxprof = max(maxprof, prices[i] - minprice);
+            ans = min(prices[i],ans);
+            maxprofit = max(maxprofit, prices[i]-ans);
         }
-        return maxprof;
+        return maxprofit;
     }
 };
