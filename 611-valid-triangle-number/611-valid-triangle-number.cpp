2a59aac82e634406ca0b3665ba15c2b7c6ad87a0
sumit8692 <sumit8692@gmail.com>
2022-05-13 02:19:39 +0530

    Time: 84 ms (96.87%), Space: 12.8 MB (91.35%) - LeetHub

--- 611-valid-triangle-number/611-valid-triangle-number.cpp
+++ 611-valid-triangle-number/611-valid-triangle-number.cpp
@@ -1,24 +1,47 @@
 class Solution {
 public:
     int triangleNumber(vector<int>& nums) {
+//         int n = nums.size();
+//         int count = 0;
+        
+//         sort(nums.begin(),nums.end());
+        
+//         for(int i = n-1;i >=2; i--){
+//             int j=0, k=i-1;
+//             while(j<k)
+//             {
+//                 if(nums[j]+nums[k]>nums[i])
+//                 {
+//                     count+=k-j;
+//                     k--;
+//                 }
+//                 else
+//                     j++;
+//             }
+//         }
+//         return count;
+        
         int n = nums.size();
-        int count = 0;
         
-        sort(nums.begin(),nums.end());
-        
-        for(int i = n-1;i >=2; i--){
-            int j=0, k=i-1;
-            while(j<k)
-            {
-                if(nums[j]+nums[k]>nums[i])
-                {
-                    count+=k-j;
-                    k--;
+        sort(nums.begin(), nums.end());
+        int ans = 0;
+        for(int i = n - 1; i >= 0; --i){
+            int first = nums[i];
+            int leftTarget = first;
+            
+            int left = 0, right = i - 1;
+            while(left < right){
+                int sum = nums[left] + nums[right];
+                if(sum > leftTarget){
+                    ans += right - left;
+                    --right;
+                } else if(sum <= leftTarget){
+                    ++left;
                 }
-                else
-                    j++;
             }
         }
-        return count;
+        
+        return ans;
+        
     }
 };
