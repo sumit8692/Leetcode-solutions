eb7c6d0a3a26b828fbd49cf2bba45fea9788c714
sumit8692 <sumit8692@gmail.com>
2022-05-14 01:04:28 +0530

    Time: 3 ms (66.80%), Space: 6 MB (26.75%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long lo = 1;
+        long hi = INT_MAX;
+        int ans = 0;
+        while(lo<=hi){
+            long long mid = hi + (lo - hi)/2;
+            if(mid*mid <= x) {
+                ans = mid;
+                lo = mid + 1;
+            }
+            
+            else hi = mid - 1;
+            
+        }
+        return (int)ans;
+    }
+};
