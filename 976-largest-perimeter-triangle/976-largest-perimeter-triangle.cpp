f546015fecdd4183ce3b012a87bea6c39ec61e23
sumit8692 <sumit8692@gmail.com>
2022-05-17 16:43:09 +0530

    Time: 39 ms (80.60%), Space: 22 MB (23.25%) - LeetHub

--- 976-largest-perimeter-triangle/976-largest-perimeter-triangle.cpp
+++ 976-largest-perimeter-triangle/976-largest-perimeter-triangle.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     int largestPerimeter(vector<int>& nums) {
         //here we just need to check that sum of two sides of a triangle is greater than the third side
-        
+        int sum = 0;
         sort(nums.begin(),nums.end());
         reverse(nums.begin(),nums.end());
         for(int i=0;i<nums.size()-2;i++){
-            if(nums[i]<nums[i+1]+nums[i+2]){
-                return nums[i]+nums[i+1]+nums[i+2];
+            sum = nums[i]+nums[i+1]+nums[i+2];
+            if(nums[i]<sum-nums[i]){
+                return sum;
             }
         }
         return 0;
