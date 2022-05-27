28b856620bd2677d7b778500001d8a346c144d7a
sumit8692 <sumit8692@gmail.com>
2022-05-27 07:38:45 +0530

    Time: 4 ms (51.15%), Space: 5.9 MB (42.35%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n == 0)  return 0;
+        int prev = 0;
+        int prev2 = 1;
+        int curr = prev + prev2;
+        
+        for(int i = 2; i <= n; i++){
+            curr = prev + prev2;
+            prev = prev2;
+            prev2 = curr;
+        }
+        
+        return curr;
+    }
+};
