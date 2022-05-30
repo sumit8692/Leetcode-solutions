6d112b0878763838fdbc29acdf8f150b563fc614
sumit8692 <sumit8692@gmail.com>
2022-05-31 00:13:20 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (67.80%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,10 +1,21 @@
+// class Solution {
+// public:
+//     // char findTheDifference(string s, string t) {
+//     //     int n = s.length();
+//     //     sort(s.begin(), s.end());
+//     //     sort(t.begin(), t.end());
+//     //     for(int i=0; i<n; i++) if(s[i]!=t[i]) return t[i];
+//     //     return t[n];
+//     // }
+    
+    
+// };
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        int n = s.length();
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        for(int i=0; i<n; i++) if(s[i]!=t[i]) return t[i];
-        return t[n];
+        char c = 0;
+        for(char cs : s) c ^= cs;
+        for(char ct : t) c ^= ct;
+        return c;
     }
 };
