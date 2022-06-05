bc6d622c987247874c18bed2cedb83ae732a6d34
sumit8692 <sumit8692@gmail.com>
2022-06-06 02:01:12 +0530

    Time: 33 ms (28.07%), Space: 19.6 MB (59.40%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,12 +1,26 @@
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         // unordered_map<int, int> counter;
+//         // for (int num : nums) {
+//         //     if (++counter[num] > nums.size() / 2) {
+//         //         return num;
+//         //     }
+//         // }
+//         // return 0;
+        
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int, int> counter;
+        int counter = 0, majority;
         for (int num : nums) {
-            if (++counter[num] > nums.size() / 2) {
-                return num;
+            if (!counter) {
+                majority = num;
             }
+            counter += num == majority ? 1 : -1;
         }
-        return 0;
+        return majority;
     }
 };
+//     }
+// };
