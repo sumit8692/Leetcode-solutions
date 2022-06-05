e1f1ac2b7cc46186ff6cef7c73df68422d99899b
sumit8692 <sumit8692@gmail.com>
2022-06-06 02:27:45 +0530

    Time: 31 ms (62.91%), Space: 31.1 MB (87.52%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-            // int pivotIndex(vector<int>& nums) {
-    // int n = nums.size();
-    // int ltmax[n];
-    // int rtmax[n];
-    // int ans = -1;
-    // ltmax[0] = nums[0];
-    // rtmax[n-1] = nums[n-1];
-    // for(int i = 1; i < n; i++){
-    //         int j = n-i-1;
-    //         ltmax[i] = ltmax[i-1] + nums[i];
-    //         rtmax[j] = rtmax[j+1] + nums[j];
-    // }
-    // for(int i = 0 ; i < n; ++i) {
-    //     if (ltmax[i] == rtmax[i])
-    //             return i;
-    //     }
-    // return -1;
-    //Noob Approach
-    int sum=0;
-    int ltsum=0;
-    for(int i = 0; i < nums.size(); i++)    
-        sum+=nums[i];
-    for(int i = 0; i<nums.size();i++){
-        if(ltsum==sum-nums[i]-ltsum) 
-            return i;
-        ltsum=ltsum+nums[i];
-    }
-    return -1;
+        long long int sum=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            sum+=nums[i];
+        }
+
+        long long int lsum=0;
+        long long int rsum= sum;
+
+        for(int i=0;i<nums.size();i++)
+        {
+            rsum = rsum-nums[i];
+
+            if(lsum==rsum)
+            {
+                return i;
+            }
+
+            lsum +=nums[i]; 
+        }
+        return -1;
     }
 };
