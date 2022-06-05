46f647eb8900d92e108d36141da2d11037ac9601
sumit8692 <sumit8692@gmail.com>
2022-06-06 02:32:11 +0530

    Time: 29 ms (69.37%), Space: 31.1 MB (53.93%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        long long int sum=0;
+        int sum=0;
         for(int i=0;i<nums.size();i++)  sum+=nums[i];
         
         int lsum=0;
