c23c485c7ec941dcd2dfe8f0fab3651f709e6852
sumit8692 <sumit8692@gmail.com>
2022-06-06 10:54:36 +0530

    Time: 28 ms (5.31%), Space: 10.7 MB (11.11%) - LeetHub

--- 414-third-maximum-number/414-third-maximum-number.cpp
+++ 414-third-maximum-number/414-third-maximum-number.cpp
@@ -3,7 +3,7 @@ public:
     int thirdMax(vector<int>& nums) {
         set<int> s;
         
-        for(int n: nums)
+        for(int &n: nums)
             s.insert(n);
         
         return s.size()<3 ? *max_element(s.begin(),s.end()) : *next(s.begin(), s.size() - 3);
