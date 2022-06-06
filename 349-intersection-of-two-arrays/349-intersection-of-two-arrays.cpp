e9a0be53ed84aebbf001b98c3baf829dedad592d
sumit8692 <sumit8692@gmail.com>
2022-06-06 10:32:01 +0530

    Time: 9 ms (49.70%), Space: 10.8 MB (17.14%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-      // put the values in set to remove duplicate
-        set<int>s1,s2;
-        vector<int>out;
-        for(auto val:nums1)
-            s1.insert(val);
-        for(auto val:nums2)
-            s2.insert(val);
+        set<int> s1, s2;
+        vector<int> res;
+        for(int num: nums1) s1.insert(num);
+        for(int num: nums2) s2.insert(num);
         
-        //find if we can get the value from first set in second set
-        for(auto val:s1)
-        {
-           if(s2.find(val)!=s2.end()) //if found push it into output vector
-               out.push_back(val);
-        }
-        return out;
+        for(int val: s1)
+            if(s2.find(val) !=s2.end())  res.push_back(val);
+        
+        return res;
     }
 };
