96bcc3f67557f5050524a29948bc7e226f9b76ce
sumit8692 <sumit8692@gmail.com>
2022-06-11 21:20:27 +0530

    Time: 307 ms (52.05%), Space: 98.6 MB (71.00%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -2,31 +2,22 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
         
-        int start =0;
-        int end =0;
-        int totalsum = accumulate(nums.begin(),nums.end(),0); 
-        int sum_to_make = totalsum - x; 
-        if(totalsum==x)return nums.size();
+        int tsum = accumulate(nums.begin(), nums.end(), 0);
+        int sum_to_make = tsum - x;
+        int st = 0, end = 0;
         int ans = 0;
-        int sum =0;
-        
-        while(end<nums.size())
-        {  sum+=nums[end]; 
-            
-         while(start<end && sum>sum_to_make) 
-         {
-             sum-=nums[start++]; 
-         }
-         if(sum==sum_to_make) 
-         {
-             ans = max(ans,end-start+1); 
-         }
-         
-           end++;
-            
+        int sum = 0;
+        if(tsum == x)    return nums.size();
+        while(end < nums.size()){
+            sum+=nums[end];
+           
+            while(sum > sum_to_make && st < end)
+                sum-=nums[st++];
+            if(sum == sum_to_make)      ans = max(ans, end-st+1);
+            end++;
         }
+
+        return ans == 0? -1: nums.size()-ans;
         
-        return ans==0 ? -1 : nums.size()-ans;
-              
     }
 };
