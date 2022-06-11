6b0feacd1e9b86e4def5f6e6c34e522cd2442a52
sumit8692 <sumit8692@gmail.com>
2022-06-12 01:04:14 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (52.47%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int len = 0;
+        int tail = s.length() - 1;
+        
+        while(s[tail] == ' ')   tail--;
+            
+        while (tail >= 0 && s[tail] != ' ') {
+            len++;
+            tail--;
+        }
+        return len;
+    } 
+};
