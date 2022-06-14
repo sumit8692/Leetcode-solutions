a15b4f67f7986b452ac009a463e7edc4477f6d44
sumit8692 <sumit8692@gmail.com>
2022-06-14 22:12:31 +0530

    Time: 15 ms (75.97%), Space: 5.9 MB (74.22%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -4,10 +4,10 @@ public:
         if(x<0) return false;//if negative return false;
         
         unsigned int temp=x,rev=0;
-        while(temp){
+        while(temp){//reverse the number 
             rev=rev*10+temp%10;
             temp/=10;
         }
-        return(x==rev);
+        return(x == rev); //check for palindrome.
     }
 };
