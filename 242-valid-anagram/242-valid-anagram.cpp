9c469dcf1bc460a394d01fac63e03e7e79d6785e
sumit8692 <sumit8692@gmail.com>
2022-06-26 17:38:37 +0530

    Time: 3 ms (98.56%), Space: 7.4 MB (45.32%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -12,12 +12,9 @@ public:
 //         return true;
         
         if(s.length() != t.length())    return false;
-        
+   
         int count[256] = {0};
-        for(int i=0;i<s.length();i++)
-            count[s[i]]++, count[t[i]]--;
-        
-        
+        for(int i=0;i<s.length();i++)   count[s[i]]++, count[t[i]]--;               
         for(int i=0;i<256;i++)  if(count[i]!=0) return false;
 
         return true;
