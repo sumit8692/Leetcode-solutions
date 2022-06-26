595e26a651fdb125d8c14f8e230231f818aea6e7
sumit8692 <sumit8692@gmail.com>
2022-06-26 16:29:33 +0530

    Time: 11 ms (69.18%), Space: 7.4 MB (45.32%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+//         if(s.length() != t.length())    return false;
+        
+//         sort(s.begin(),s.end());
+//         sort(t.begin(),t.end());
+        
+//         for(int i = 0; i < s.size(); i++){
+//             if(s[i] != t[i])    return false;
+//         }
+//         return true;
+        
+        if(s.length() != t.length())    return false;
+        
+        int count[256] = {0};
+        for(int i=0;i<s.length();i++) {
+            count[s[i]]++;
+            count[t[i]]--;
+        }
+        
+        for(int i=0;i<256;i++)  if(count[i]!=0) return false;
+
+        return true;
+        
+    } 
+};
