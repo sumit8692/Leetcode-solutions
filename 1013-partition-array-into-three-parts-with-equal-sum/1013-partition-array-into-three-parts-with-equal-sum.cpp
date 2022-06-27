7be586398ff22cc35f8e17044b7afb697a7ec67f
sumit8692 <sumit8692@gmail.com>
2022-06-28 04:10:54 +0530

    Time: 60 ms (74.19%), Space: 32.6 MB (62.22%) - LeetHub

---
+++ 1013-partition-array-into-three-parts-with-equal-sum/1013-partition-array-into-three-parts-with-equal-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool canThreePartsEqualSum(vector<int>& arr) {
+        
+        int sum = 0;
+        for(auto a: arr)    sum += a;
+        
+        if(sum % 3 != 0)    return false;
+        
+        int each = sum/3;
+        int temp = 0;
+        int found = 0;
+        
+        for(auto a: arr){
+            temp+=a;
+            if(temp == each)    found++, temp = 0;
+        }
+        
+        return found >= 3?1:0;
+
+    }
+};
