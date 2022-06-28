0dede134b9057a68597667f1527e05903c63e27a
sumit8692 <sumit8692@gmail.com>
2022-06-28 22:25:25 +0530

    Added solution - LeetHub

--- The Celebrity Problem - GFG/the-celebrity-problem.cpp
+++ The Celebrity Problem - GFG/the-celebrity-problem.cpp
@@ -17,9 +17,9 @@ class Solution
         vector<int> cel;
         int ct = 0, ans = -1;
         for(int i = 0; i < n; i++)  cel.push_back(0);
-        for(int i = 0; i < n; i++){
-            if(M[i] == cel) ct++, ans = i;
-        }
+        for(int i = 0; i < n; i++)  if(M[i] == cel) ct++, ans = i;
+            
+
         if(ct != 1) return -1;
         return ans;
     }
