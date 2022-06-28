e023d206f3a69db0f79eaba8afc1da169a2c6d56
sumit8692 <sumit8692@gmail.com>
2022-06-28 18:10:18 +0530

    Time: 50 ms (11.52%), Space: 21.5 MB (67.47%) - LeetHub

--- 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.cpp
+++ 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.cpp
@@ -4,7 +4,7 @@ public:
         
         for(int i = 0, j = 1; i < nums.size();){
             if(nums[i]%2==0)   i+=2;
-            else if(nums[j]%2!=0)   j+=2; 
+            else if(nums[j]%2==1)   j+=2; 
             else swap(nums[i],nums[j]);
         }
         return nums;
