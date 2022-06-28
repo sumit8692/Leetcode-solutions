67ced8584823630659f23c6f42200c5047d9b10b
sumit8692 <sumit8692@gmail.com>
2022-06-28 07:36:57 +0530

    Time: 660 ms (21.43%), Space: 0B (100.00%) - LeetHub

---
+++ 181-employees-earning-more-than-their-managers/181-employees-earning-more-than-their-managers.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select a.Name AS 'Employee'
+from 
+    Employee AS a,
+    Employee AS b
+where
+    a.ManagerId = b.Id
+            AND a.Salary > b.Salary;
+            
