5cc2715af1c604cd79cafbf3277c319f408e1e19
sumit8692 <sumit8692@gmail.com>
2022-07-03 03:45:15 +0530

    Added solution - LeetHub

---
+++ Find unique element - GFG/find-unique-element.cpp
@@ -0,0 +1,36 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+class Solution{
+  public:
+    int findUnique(int a[], int n, int k) {
+        unordered_map<int, int> mp;
+        
+        for(int i = 0; i < n; i++)  mp[a[i]]++;
+        
+        for(auto it: mp)
+            if(it.second%k != 0)    return it.first;
+        
+    
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int a[n], i;
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution obj;
+        cout << obj.findUnique(a, n, k) << endl;
+    }
+}  // } Driver Code Ends
