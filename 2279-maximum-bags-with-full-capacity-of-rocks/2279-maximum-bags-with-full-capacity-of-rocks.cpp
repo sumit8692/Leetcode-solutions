d795c41f5f2381e02debb61b5606d09a098f5fce
sumit8692 <sumit8692@gmail.com>
2022-07-03 00:45:46 +0530

    Time: 299 ms (45.16%), Space: 84.6 MB (98.27%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
         
-        for(int i = 0; i < capacity.size(); i++)    capacity[i] -= rocks[i];
+        for(int i = 0; i < capacity.size(); i++)    capacity[i] -= rocks[i];// to check which bags are already filled if it's 0 meaning bags filled otherwise we will have to fill it with additional rocks
         
         int cnt = 0;
-        sort(capacity.begin(), capacity.end());\
+        sort(capacity.begin(), capacity.end());//
             
         for(int i = 0; i < capacity.size(); i++){
             if(capacity[i] == 0)  cnt++;
             else if(capacity[i] <= additionalRocks) cnt++, additionalRocks -= capacity[i];
-            
+            else break;
         }
         
         return cnt;
