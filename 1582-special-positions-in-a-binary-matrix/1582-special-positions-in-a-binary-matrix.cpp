3bec8a157f06e5234653070d246a3070cc41b080
sumit8692 <sumit8692@gmail.com>
2022-07-03 17:09:53 +0530

    Time: 52 ms (17.32%), Space: 12.9 MB (78.75%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
@@ -11,19 +11,17 @@ public:
         for(int i = 0; i < row*col; i++)
             r[i/col] = 0,   c[i%col] = 0;
         
-        for(int i = 0; i < row*col; i++){
-            if(mat[i/col][i%col] == 1){
-                r[i/col]++;
-                c[i%col]++;
-            }
-        }
-        
+        for(int i = 0; i < row*col; i++)
+            if(mat[i/col][i%col] == 1)
+                r[i/col]++, c[i%col]++;
+
         int count = 0;
         
         for(int i = 0; i < row*col; i++)
             if(mat[i/col][i%col] == 1 && r[i/col] == 1 && c[i%col] == 1)
                 count++;
         
-     return count;   
+        return count;   
+        
     }
 };
