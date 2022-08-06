f9426c3c43011f4c28ae7a3c4755b46d3474c769
sumit8692 <sumit8692@gmail.com>
2022-08-07 04:52:29 +0530

    Time: 4 ms (25.82%), Space: 6.1 MB (22.55%) - LeetHub

---
+++ 458-poor-pigs/458-poor-pigs.cpp
@@ -0,0 +1,9 @@
+#include<math.h>
+class Solution {
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        
+        return ceil((log(buckets))/(log(minutesToTest/minutesToDie+1)));
+        
+    }
+};
