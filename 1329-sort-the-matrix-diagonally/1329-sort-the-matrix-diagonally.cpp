4b5c575c6e32e7ea00e6fc39b8ab7e62d2aeadc3
sumit8692 <sumit8692@gmail.com>
2022-08-29 02:38:04 +0530

    Time: 16 ms (63.80%), Space: 9.3 MB (52.62%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -4,6 +4,7 @@ public:
         
         unordered_map<int, priority_queue<int, vector<int>, greater<int>>> map;
         int m = mat.size(), n = mat[0].size();
+        
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 map[i-j].push(mat[i][j]);
