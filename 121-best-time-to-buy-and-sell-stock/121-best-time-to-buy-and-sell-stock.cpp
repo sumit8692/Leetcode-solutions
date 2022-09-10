33ab009ee15695ada5a8e674446618aba83d5ca6
sumit8692 <sumit8692@gmail.com>
2022-09-10 21:05:43 +0530

    Time: 341 ms (5.02%), Space: 93.2 MB (99.06%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -5,10 +5,12 @@ public:
         //int st = 0;
         int ans = INT_MAX;
         int maxprofit = 0;
-        int minprice = INT_MAX;
+        
         for(int i = 0; i < prices.size(); i++){
+            
             ans = min(prices[i],ans);
             maxprofit = max(maxprofit, prices[i]-ans);
+            
         }
         return maxprofit;
     }
