e93207c6634c7a9001011dbd36db4077ecfa6ba2
sumit8692 <sumit8692@gmail.com>
2022-09-13 03:01:27 +0530

    Time: 8 ms (72.74%), Space: 10.6 MB (67.18%) - LeetHub

--- 948-bag-of-tokens/948-bag-of-tokens.cpp
+++ 948-bag-of-tokens/948-bag-of-tokens.cpp
@@ -3,14 +3,18 @@ public:
     int bagOfTokensScore(vector<int>& tokens, int power) {
        
         sort(tokens.begin(), tokens.end());
+        
         int i, j, n;
+        
         i = 0; j = n = tokens.size();
         
         while(i < j){
             if(power >= tokens[i])  power -= tokens[i++];
             
             else if(i - (n - j) && j > i+1) power += tokens[--j];
+            
             else break;
+            
         }
         return i - (n - j);
     }
